a110c2646f5c7f56f8bef2afbbcdaf7f6155cd3f
schnetzi <paul.schnetzinger@gmail.com>
2019-10-04 16:23:44 +0100

    Initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <button @click="onPrint">Print</button>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Vue from "vue";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
 export default Vue.extend({
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld,
+    HelloWorld
   },
+  methods: {
+    onPrint() {
+      window.print();
+    }
+  }
 });
 </script>
